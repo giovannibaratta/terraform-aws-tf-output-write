6610deef0a3a84e78c96ba2e3c62e1687c7bba4f
Giovanni Baratta <bargiovi@hotmail.it>
2024-05-29 15:46:43 +0200

    Fix typo in aws_s3

--- aws_s3.tf
+++ aws_s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_object" "data" {
-  bucket = var.destinations.aws_s3.bucket_id
-  key    = var.destinations.aws_s3.object_path
+  bucket = var.destination.aws_s3.bucket_id
+  key    = var.destination.aws_s3.object_path
 
   content = local.content
 }
